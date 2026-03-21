7ccd6f38994c7a97c7a9b2913d48ab430959edd2
ImBad <70207769+neofilisoft@users.noreply.github.com>
2026-03-21 13:47:06 +0700

    Delete scripts directory

--- scripts/build.ps1
+++
@@ -1,9 +0,0 @@
-﻿param(
-    [string]$BuildDir = "build"
-)
-
-$root = Resolve-Path (Join-Path $PSScriptRoot "..")
-$buildPath = Join-Path $root $BuildDir
-
-cmake -S $root -B $buildPath
-cmake --build $buildPath --config Release
